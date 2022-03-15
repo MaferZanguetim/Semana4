3a21b61cdeb954aa162c47b9838b086e2d708951
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-15 12:33:28 +0000

    Exercicio4

---
+++ Exercicio4.java
@@ -0,0 +1,10 @@
+public class Exercicio4{
+    public static void main(String[] args){
+        int fatorial=1;
+        for(int n = 1; n <= 10; n++){
+            fatorial = n*fatorial;
+            System.out.println(fatorial);
+        }
+
+    }
+}
