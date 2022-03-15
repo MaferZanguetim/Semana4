5bcdf0dc728ab808460cc9957fad7873d6cae4cb
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-15 12:03:38 +0000

    Exercicio1

---
+++ Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1{
+    public static void main(String[] args){
+        int x = 150;
+        while (x<301){
+            System.out.println("Número atual: "+ x);
+            x++;
+        }
+    }
+} 
