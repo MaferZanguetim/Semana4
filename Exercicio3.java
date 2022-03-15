b1c18da8251245449489b90ab10d3062fbdc7f9c
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-15 12:23:47 +0000

    Exercicio3

---
+++ Exercicio3.java
@@ -0,0 +1,9 @@
+public class Exercicio3{
+    public static void main (String[] args){
+        for (int p=1; p<=100; p++){
+            if(p%3==0){
+                System.out.println(p);
+            }
+        }
+    }
+}
