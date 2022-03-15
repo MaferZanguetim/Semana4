3f27a230b3af08991f7f28dcdaf2dc2ae833c69e
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-15 12:15:08 +0000

    Exercicio2

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+public class Exercicio2{
+    public static void main(String[] args){
+        int a=1;
+        for(int n=1; n<=1000; n++){
+            a=a+n;
+            System.out.println(a);
+        } 
+       
+    }
+}
